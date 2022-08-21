e9fed0ae6b10e07761a60e21ec1b969311475f1f
zhoujingking <85977400+zhoujingking@users.noreply.github.com>
2022-08-21 20:15:38 +0800

    demo: track mouse position

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,10 @@
 <script setup>
-import {reactive, ref, computed, onMounted, onBeforeMount, onActivated} from 'vue'
-const state = reactive({
-  list: ['a', 'b', 'c']
-})
-
-const listCount = computed(() => {
-  return state.list.length;
-})
-
-function onClick() {
-  state.list.push('d')
-}
-
-onBeforeMount(() => {
-  console.log('before mount')
-})
-
-onMounted(() => {
-   console.log('mounted')
-})
-
-onActivated(() => {
-  console.log('activated')
-})
+import { useMouse } from '@vueuse/core'
+const {x, y} = useMouse();
 </script>
 
 <template>
-  <button @click="onClick">test</button>
-  <div>{{listCount}}</div>
+  <div>x: {{x}}, y: {{y}}</div>
 </template>
 
 <style scoped>
