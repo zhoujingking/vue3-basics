850a192106feb91120ea27a3030cd867b812d7f0
zhoujingking <85977400+zhoujingking@users.noreply.github.com>
2022-08-21 20:03:21 +0800

    vite-vue setup

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script setup>
+import {reactive, ref, computed, onMounted, onBeforeMount, onActivated} from 'vue'
+const state = reactive({
+  list: ['a', 'b', 'c']
+})
+
+const listCount = computed(() => {
+  return state.list.length;
+})
+
+function onClick() {
+  state.list.push('d')
+}
+
+onBeforeMount(() => {
+  console.log('before mount')
+})
+
+onMounted(() => {
+   console.log('mounted')
+})
+
+onActivated(() => {
+  console.log('activated')
+})
+</script>
+
+<template>
+  <button @click="onClick">test</button>
+  <div>{{listCount}}</div>
+</template>
+
+<style scoped>
+</style>
---
+++ src/main.js
@@ -0,0 +1,4 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+const rootAppComp = createApp(App)
+rootAppComp.mount('#app')
